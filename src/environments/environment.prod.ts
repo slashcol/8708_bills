71d7c5a2ef9bd081b7dd2942e171e8b05a290cdc
unknown <g1770@hotmail.com>
2020-11-29 11:50:53 -0500

    Etapa de prueba nov30

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,17 @@
+export const environment = {
+  production: true,
+  serverUrl: 'https://admin.tryion.shop/api',
+  appUrl: 'https://www.tryion.shop',
+  appImageUrl: 'https://www.tryion.shop/assets/imgs/ionshop.png',
+  appId: 'JrWy7sUKLL',
+  fbId: 'YOUR_FACEBOOK_APP_ID',
+  stripePublicKey: 'YOUR_STRIPE_PUBLIC_KEY',
+  androidHeaderColor: '#222428',
+  defaultLang: 'en',
+  googleClientId: 'YOUR_GOOGLE_CLIENT_ID',
+  currency: {
+    code: 'USD',
+    display: 'symbol',
+    digitsInfo: '1.2-2',
+  },
+};
