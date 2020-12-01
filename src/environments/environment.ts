a1996f22331dd3b31d1bebf52deb8b04e202ca94
unknown <g1770@hotmail.com>
2020-12-01 08:29:34 -0500

    Etapa de prueba dic_1

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,7 +6,7 @@ export const environment = {
   production: false,
   serverUrl: 'https://ionshop.bills.pet/api',
   appUrl: 'https://www.bills.pet',
-  appImageUrl: 'https://www.tryion.shop/assets/imgs/ionshop.png',
+  appImageUrl: 'https://www.bills.pet/assets/imgs/ionshop.png',
   appId: 'hcDzr5W7Qt',
   fbId: '789454091616641',
   stripePublicKey: 'pk_test_51HFhRDGzSWOHkwi6XGvnmqsEpLwBIKDkvKv4jyrHmn15Iv0toDWelpolk1i5wVLPjz9lA3P4cotfRYxENfZmZLnr00aMVGHo0T',
